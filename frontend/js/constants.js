57a12d66cf3bf35d971d8087436133d2b96dfd87
PSFSho <100551992+PSFSho@users.noreply.github.com>
2022-03-29 22:41:11 -0500

    addy

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xE7FD841dD9537b56A66B274a9715d0de651C2c82"; //Replace with your own contract address
 const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Psychedelic Shroom Forest NFT Project!!";
